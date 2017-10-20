443762915f10911144cc75d0e18e7e0d055202f7
James Dela Cruz <jcruz@assemblypayments.com>
2017-10-20 13:32:15 +0800

    Modify layouts

--- web/router.ex
+++ web/router.ex
@@ -31,7 +31,7 @@ defmodule Chatter.Router do
   scope "/", Chatter do
     pipe_through [:browser, :browser_auth]
     resources "/users", UserController, only: [:index, :show, :new, :edit, :delete, :update]
-    get "/chat", PageController, :index
+    get "/games", PageController, :index
   end
 
 
